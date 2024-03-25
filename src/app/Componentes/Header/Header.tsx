95c066b5c42e38ff51b82f625b72f5e4ab455651
LuisCetauro <cetaurot@gmail.com>
2024-03-25 00:46:52 -0300

    Frist

--- src/app/Componentes/Header/Header.tsx
+++ src/app/Componentes/Header/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="flex flex-row gap-10 bg-black h-14 text-white text-sm  border-red-500 border-b-8">
+    <div className="flex flex-row gap-12 bg-black h-14 text-white text-sm  border-red-500 border-b-8">
       <Link href="/">
         <div className="ml-3 mt-2">
           <p>Home</p>
@@ -11,7 +11,7 @@ export default function Header() {
 
       <Link href="/Aps">
         <div className="mt-2">
-          <p>Veja nossos Apartamentos</p>
+          <p>Apartamentos</p>
         </div>
       </Link>
 
--- src/app/Componentes/InfoVendedor/InfoVendedor.tsx
+++ src/app/Componentes/InfoVendedor/InfoVendedor.tsx
@@ -6,17 +6,14 @@ export default function InfosDoVendedor() {
   const VendedorMockUp = {
     nome: "Luis Cetauro",
     desc: "Olá, estou tentando engressar como o mais novo colaborador da Direcional Vendas e aqui está uma breve demonstração do que eu posso trazer para o time de Brasília.",
-    foto: "/Eu.jpeg",
+    foto: "/Eu2.png",
   };
 
   return (
     <div className="flex flex-col justify-center items-center w-3/5 ml-20 mt-12 gap-4">
-      <Image
-        height={1}
-        width={96}
-        src={VendedorMockUp.foto}
-        alt="Um homem vestido de terno"
-      />
+      <div className="relative w-40 h-40">
+        <Image fill src={VendedorMockUp.foto} alt="Um homem vestido de terno" />
+      </div>
       <p>{VendedorMockUp.nome}</p>
       <p>{VendedorMockUp.desc}</p>
     </div>
